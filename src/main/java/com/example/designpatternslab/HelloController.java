9988dce6b674224071e43d65fb18bf493fb14d8e
IonSeremet <ionseremet@gmail.com>
2023-12-18 12:59:02 +0200

    Implemented Dependency Injection

---
+++ src/main/java/com/example/designpatternslab/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.designpatternslab;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class HelloController {
+
+    private final ClientComponent clientComponent;
+
+    @GetMapping
+    public String hello() {
+        return "Hello from ClientComponent = " + clientComponent;
+    }
+}
